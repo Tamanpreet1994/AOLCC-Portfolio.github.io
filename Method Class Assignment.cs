10ba344536034e7156750f88cb6a2c0b21bd2150
Tamanpreet1994 <kaurtaman64@gmail.com>
2025-02-22 20:11:10 -0500

    Add files via upload

---
+++ Method Class Assignment.cs
@@ -0,0 +1,31 @@
+﻿
+public class MathOperations
+{
+
+    public void PerformOperation(int num1, int num2)
+    {
+
+        int result = num1 + 10;
+
+
+        Console.WriteLine($"Result of {num1} + 10 = {result} and second number is {num2}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        MathOperations mathOp = new MathOperations();
+
+
+        mathOp.PerformOperation(5, 20);  // First number 5, second number 20
+
+
+        mathOp.PerformOperation(num1: 10, num2: 30);  // First number 10, second number 30
+
+
+        Console.ReadKey();
+    }
+}
