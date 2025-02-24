1bc2e85030d43156e2c36d6c0bac498e59a2bd19
Tamanpreet1994 <kaurtaman64@gmail.com>
2025-02-23 22:11:25 -0500

    Add files via upload

---
+++ Polymorphism Assignment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Polymorphism_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+interface IQuittable
+{
+    void Quit(); 
+}
+
+
+class Employee : IQuittable
+{
+ 
+    public string Name { get; set; }
+
+ 
+    public Employee(string name)
+    {
+        Name = name;
+    }
+
+   
+    public void Quit()
+    {
+        Console.WriteLine($"Employee {Name} has quit the job.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Employee employee = new Employee("John Doe");
+
+       
+        IQuittable quittableEmployee = employee;
+
+       
+        quittableEmployee.Quit();
+
+       
+        Console.ReadLine();
+    }
+}
